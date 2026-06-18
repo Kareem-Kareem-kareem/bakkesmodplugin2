19c52e8e6678734967053b36308e1456597ef7da
Kareem-Kareem-kareem <kareemabidin1@gmail.com>
2026-06-18 12:08:30 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,26 @@
-#include <string>
-#include <vector>
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 
-class SimpleGreeter : public BakkesMod::Plugin::BakkesModPlugin
+class HelloPlugin : public BakkesMod::Plugin::BakkesModPlugin
 {
 public:
     void onLoad() override
     {
-        cvarManager->registerCvar("greeter_message", "Hello from BakkesMod!",
-            "The message printed by the 'greet' command");
-
-        cvarManager->registerCommand("greet", "Prints the greeting",
-            [this](const std::vector<std::string>&) {
-                std::string msg = cvarManager->getCvar("greeter_message").getStringValue();
-                cvarManager->log("GREETER: " + msg);
-            });
-
-        cvarManager->log("SimpleGreeter loaded. Type 'greet'.");
+        cvarManager->log("HelloPlugin loaded!");
     }
-
     void onUnload() override
     {
-        cvarManager->log("SimpleGreeter unloaded.");
+        cvarManager->log("HelloPlugin unloaded.");
     }
 };
 
 extern "C" __declspec(dllexport) BakkesMod::Plugin::BakkesModPlugin* CreatePlugin()
 {
-    return new SimpleGreeter();
+    return new HelloPlugin();
 }
 
 extern "C" __declspec(dllexport) const char* GetPluginName()
 {
-    return "SimpleGreeter";
+    return "HelloPlugin";
 }
 
 extern "C" __declspec(dllexport) unsigned long GetPluginVersion()
